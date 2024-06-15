e4a87882e259ae0859f94cc6f75cb3e4d4452e1d
Tushar <tusharimran470@gmail.com>
2024-06-15 15:54:28 +0600

    delete api added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tgzt8q2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -60,6 +60,14 @@ app.get('/products', async(req,res)=>{
     res.send(result)
 
 })
+
+app.delete('/products/:id',async(req,res)=>{
+     const id= req.params.id;
+     const query={_id:new ObjectId(id)}
+     const result= await productcollection.deleteOne(query)
+     res.send(result)
+}
+)
 
 
 
