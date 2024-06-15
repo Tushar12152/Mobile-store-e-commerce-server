dcb73b5daaa10e1ffc4193f2f8339110d4a49de2
Tushar <tusharimran470@gmail.com>
2024-06-15 20:50:54 +0600

    single data get from database

--- index.js
+++ index.js
@@ -66,8 +66,14 @@ app.delete('/products/:id',async(req,res)=>{
      const query={_id:new ObjectId(id)}
      const result= await productcollection.deleteOne(query)
      res.send(result)
-}
-)
+})
+
+app.get('/products/:id',async (req,res)=>{
+     const id=req.params.id;
+     const query={_id:new ObjectId(id)}
+     const result=await productcollection.findOne(query)
+     res.send(result)
+})
 
 
 
